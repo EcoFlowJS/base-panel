271f82fbb8f8e46255a8e318090aeb7819d7dfea
RomelSikdar <romels500@gmail.com.com>
2024-01-02 14:49:46 +0530

    .

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/auth",
+  server: {
+    port: 3000,
+    proxy: {
+      "/admin": {
+        target: "http://localhost:3001/",
+        changeOrigin: true,
+      },
+      "/editor/flow": {
+        target: "http://localhost:3002/",
+        changeOrigin: true,
+      },
+      "/editor/schema": {
+        target: "http://localhost:3003/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
