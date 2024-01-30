4e4f394fb47dfcbe8fc5f94ad57b2cc0cdd90b95
RomelSikdar <romels500@gmail.com.com>
2024-01-30 15:58:58 +0530

    Dashboard Redirect Fix

--- src/components/Dashboard/content/Contents.component.tsx
+++ src/components/Dashboard/content/Contents.component.tsx
@@ -8,10 +8,6 @@ import Button from "./button/Button";
 import { Link } from "react-router-dom";
 
 export default function DashboardContents() {
-  const redirect = (url: string) => () => {
-    window.location.replace(window.location.origin + url);
-  };
-
   return (
     <FlexboxGrid
       justify="center"
@@ -26,14 +22,15 @@ export default function DashboardContents() {
       </FlexboxGrid.Item>
       <FlexboxGrid.Item>
         <Stack spacing={20} style={{ paddingTop: "6rem" }}>
-          <Button
-            appearance="primary"
-            icon={<RiAdminLine />}
-            labletext="Admin Panel"
-            style={styles.IconButton}
-            circle
-            onClick={redirect("/admin")}
-          />
+          <Link to={{ pathname: "/admin" }} target="_parent">
+            <Button
+              appearance="primary"
+              icon={<RiAdminLine />}
+              labletext="Admin Panel"
+              style={styles.IconButton}
+              circle
+            />
+          </Link>
           <Link to={{ pathname: "/editor/schema" }} target="_blank">
             <Button
               color="orange"
