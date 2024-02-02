ab6e3ac57b689b6623bdc6477844a23f0b26174a
RomelSikdar <romels500@gmail.com.com>
2024-02-02 08:33:17 +0530

    init service updated

--- src/layout/BaseLayout/BaseLayout.layout.tsx
+++ src/layout/BaseLayout/BaseLayout.layout.tsx
@@ -2,15 +2,19 @@ import { Outlet } from "react-router-dom";
 import initService from "../../service/init/init.service";
 import redirect from "../../utils/redirect/redirect";
 import { useEffect } from "react";
+import { useAtom } from "jotai";
+import initStatus from "../../store/initStatus.store";
 
 export default function BaseLayout() {
-  const ststus = initService();
+  const status = initService();
   const re = redirect();
+  const [_initStatus, setinitStatus] = useAtom(initStatus);
 
   useEffect(() => {
-    if (ststus.isNew && !ststus.isLoggedIn) re("setup");
-    if (!ststus.isNew && !ststus.isLoggedIn) re("login");
-    if (!ststus.isNew && ststus.isLoggedIn) re("dashboard");
+    setinitStatus({ ...status });
+    if (status.isNew && !status.isLoggedIn) re("setup");
+    if (!status.isNew && !status.isLoggedIn) re("login");
+    if (!status.isNew && status.isLoggedIn) re("dashboard");
   }, []);
 
   return <Outlet />;
